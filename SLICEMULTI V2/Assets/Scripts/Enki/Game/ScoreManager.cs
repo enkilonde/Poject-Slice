6de336d841856a64ac6245e99642818c4bbdb285
EnkiLonde <enkilonde@gmail.com>
2015-04-23 14:50:28 +0200

    update, removed spawning bug

--- SLICEMULTI V2/Assets/Scripts/Enki/Game/ScoreManager.cs
+++ SLICEMULTI V2/Assets/Scripts/Enki/Game/ScoreManager.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour 
 {
 
+	public int _LocalScore = 0;
+	public Text _ScoreText;
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+		_ScoreText = transform.Find ("Canvas").Find ("Score").GetComponent<Text> ();
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		_ScoreText.text = _LocalScore.ToString ();
+
+
 	}
 }
