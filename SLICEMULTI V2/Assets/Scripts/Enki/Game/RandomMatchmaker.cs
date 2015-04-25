011f8f972b115603954bcf9fe816ebc220d1bf60
EnkiLonde <enkilonde@gmail.com>
2015-04-25 15:25:46 +0200

    save

--- SLICEMULTI V2/Assets/Scripts/Enki/Game/RandomMatchmaker.cs
+++ SLICEMULTI V2/Assets/Scripts/Enki/Game/RandomMatchmaker.cs
@@ -60,7 +60,6 @@ public class RandomMatchmaker : MonoBehaviour {
 		_Player.name = _LManager._PlayerName;
 		PhotonNetwork.player.name = _LManager._PlayerName;
 		_Player.transform.Find("PlayerName").Find("Text").GetComponent<Text>().text = _LManager._PlayerName;
-	
 		//_OManager._PhotonView.RPC ("updatePlayerList", PhotonTargets.All);
 
 	}
